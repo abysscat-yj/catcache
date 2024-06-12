e544cd28fefe038f5fa538ea565959dcb97d7343
abysscat-yj <397967552@qq.com>
2024-06-12 23:23:13 +0800

    init

---
+++ src/main/java/com/abysscat/catcache/CatcacheApplication.java
@@ -0,0 +1,13 @@
+package com.abysscat.catcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatcacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatcacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abysscat/catcache/CatcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abysscat.catcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatcacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
